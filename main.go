f3aed154dd098e70839b33cfcffb79d2a5cefa63
Sanjay Bhattacharya <sanjbh@gmail.com>
2025-07-27 08:00:02 +0530

    boilerplate cleanup

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.Run()
-}
-
-type Recipe struct {
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
 }
